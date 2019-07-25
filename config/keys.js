15f06d08e0521f45b77d6231d38c50eeba8175cb
Aoaltawashi <aoaltawashi@gmail.com>
2019-07-25 06:46:34 +0300

    Second Commit

---
+++ config/keys.js
@@ -0,0 +1,17 @@
+module.exports = {
+   // mongoURI: 'mongodb+srv://AOALTAWASHI:Aa0533381842@cluster0-l5682.mongodb.net/test?retryWrites=true&w=majority' 
+    mongoURI: 'mongodb+srv://aoaltawashi:Aa0533381842@cluster0-l5682.mongodb.net/test' 
+
+  //  mongodb+srv://aoaltawashi:Aa0533381842@cluster0-l5682.mongodb.net/test
+}
+
+
+//const MongoClient = require('mongodb').MongoClient;
+//const uri = 'mongodb+srv://AOALTAWASHI:Aa0533381842@cluster0-l5682.mongodb.net/test?retryWrites=true&w=majority';
+//const client = new MongoClient(mongoURI, { useNewUrlParser: true });
+//client.connect(err => {
+  //const collection = client.db("test").collection("devices");
+ // perform actions on the collection object
+  //client.close();
+//});
+//*/
